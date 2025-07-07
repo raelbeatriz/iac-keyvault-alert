569b978ea0a35f1e53f9238c1aec1d8dd6b3ee9a
Rael Beatriz Nabong <105956144+raelbeatriz@users.noreply.github.com>
2025-07-07 17:41:14 +0800

    Create locals.tf

---
+++ keyvault2/locals.tf
@@ -0,0 +1,65 @@
+locals {
+  tags = {
+    cloud               = "Azure"
+    country             = "DDC"
+    market              = "global"
+    platform            = "DXP"
+    department          = "DXP1"
+    environment         = "dev"
+    geographic_location = "eun"
+    project_description = "DXP Expiration Alert"
+    project_name        = "dxp-expiration-alert"
+    project_owner       = "tbd"
+    solution_name       = "tbd"
+    cost_center         = "devops"
+    source              = "terraform"
+  }
+
+rg_name   = "az-eun-ddc-rg-tf-dxp-mgmt"
+kv_name   = "az-eun-ddc-kv-dxp-mgmt"
+law_name  = "az-eun-ddc-log-dxp-mgmt"
+law_rg    = "az-eun-ddc-rg-tf-dxp-mgmt"
+
+#diagnostic setting
+ds_name   = "expiration-alert"
+
+#action group
+ag_name           = "expiration-alert"
+ag_short_name     = "Alert"
+email_addresses = {
+  "sendtonikki"    = ""
+  "sendtojacque"   = ""
+  "sendtojd"       = ""
+  "sendtorael"     = ""
+  "sendtocyril"    = ""
+  "sendtojaime"    = ""
+  "sendtokevin"    = ""
+  "sendtoangelo"   = ""
+  "sendtojaime2"   = ""
+  "sendtoyesid"    = ""
+}
+
+#alert rule (secret)
+ar_name_secret               = "expiring-secret"
+evaluation_frequency_secret  = "P1D"
+window_duration_secret       = "P1D"
+severity_secret              = 3
+description_secret           = "Expiring Secret"
+display_name_secret          = "expiring-secret"
+
+#alert rule (key)
+ar_name_key               = "expiring-key"
+evaluation_frequency_key  = "P1D"
+window_duration_key       = "P1D"
+severity_key              = 3
+description_key           = "Expiring Key"
+display_name_key          = "expiring-key"
+
+#alert rule (certificate)
+ar_name_cert                = "expiring-certificate"
+evaluation_frequency_cert   = "P1D"
+window_duration_cert        = "P1D"
+severity_cert               = 3
+description_cert            = "Expiring Certificate"
+display_name_cert           = "expiring-certificate"
+}
