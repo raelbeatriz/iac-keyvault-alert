0d3bb097b69726c7daa736fbf8a54a0ba145cb07
Rael Beatriz Nabong <105956144+raelbeatriz@users.noreply.github.com>
2025-07-07 17:52:22 +0800

    Update backend.tf

--- keyvault2/backend.tf
+++ keyvault2/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "az-eun-ddc-rg-tf-dxp-mgmt"
-    storage_account_name = "azeunddcstdxpmgmt"
-    container_name       = "dxpsecretalert"
-    key                  = "az-eun-ddc-kv-dxp-mgmt-secret-alert.terraform.tfstate"
+    resource_group_name  = ""
+    storage_account_name = ""
+    container_name       = ""
+    key                  = ""
   }
 }
